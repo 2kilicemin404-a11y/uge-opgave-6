72a272ab4b959cca58ad975d2e136135543aee08
eminkilic <kilicemin404@gmail.com>
2026-03-12 23:32:57 +0100

    super svært! classes lavet, interfaces

---
+++ .idea/src/MediaItem.java
@@ -0,0 +1,22 @@
+public abstract class MediaItem{
+    String title;
+    int durationSeconds;
+    MediaItem(String title, int durationSeconds){
+        this.title = title;
+        this.durationSeconds = durationSeconds;
+
+
+    }
+   public String getTitle(){
+return title;
+
+    }
+   public int getDurationSeconds(){
+        return durationSeconds;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Duration: " + durationSeconds;
+    }
+}
---
+++ .idea/src/Song.java
@@ -0,0 +1,15 @@
+public class Song extends MediaItem implements Playable {
+    String artist;
+
+    Song(String artist, String title, int durationSeconds){
+        super(title, durationSeconds);
+        this.artist = artist;
+
+
+
+    }
+    @Override
+   public void play(){
+        System.out.println("♪ Afspiller: " + getTitle() + " af " + artist);
+    }
+}
